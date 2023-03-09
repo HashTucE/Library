0e75e31889c8c46e8f87cb32fb7bddf6b0345307
HashTucE <jeremyb59@wanadoo.fr>
2023-03-09 11:21:39 +0100

    Replacement by spring boot strater validation

--- src/main/java/com/mediscreen/library/dto/PatientDto.java
+++ src/main/java/com/mediscreen/library/dto/PatientDto.java
@@ -1,5 +1,7 @@
 package com.mediscreen.library.dto;
 
+
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
